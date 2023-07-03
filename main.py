7361482ae74ec8ea1c6bc3b0c753d87da2f7b845
PratikHotchandani <pratikhot123@live.com>
2023-07-03 12:50:10 -0400

    added code to get match data from seasons

--- main.py
+++ main.py
@@ -9,32 +9,44 @@ from mplsoccer import Pitch, Sbopen
 parser = Sbopen()
 
 st.subheader("Here, we have La Liga data of all the Matches played by Barcelona in the Lionel Messi era")
+df_competition = parser.competition()
+df_selected_season_matches = pd.DataFrame()
 
-def getSeasonDict():
-    df_competition = parser.competition()
+def getSeasonDict():    
     df_laliga_rows = df_competition[df_competition['competition_id'] == 11]
     season_name = df_laliga_rows['season_name']
     season_id = df_laliga_rows['season_id']
     season_dict = dict(zip(season_name, season_id))
     return season_dict
 
+# Function to fetch matches based on 'competition_id' and 'season_id' values
+def fetch_matches(row):
+    competition_id = row['competition_id']
+    season_id = row['season_id']
+    # Your logic or function based on 'competition_id' and 'season_id' values
+    df_match = parser.match(competition_id=competition_id, season_id=season_id)
+    return df_match
+
+
+def getSelectedSeasonMatchData(selected_season):
+    desired_season = season_dict[selected_season]
+    df_laliga_rows = df_competition[(df_competition['competition_id'] == 11) & (df_competition['season_id'] == desired_season)]
+    
+    # Iterate through the rows and fetch matches
+    for index, row in df_laliga_rows.iterrows():
+        matches = fetch_matches(row)
+        # Append the matches to df_championsLeague_matches
+        df_selected_season_matches = df_selected_season_matches.append(matches, ignore_index=True)
+
+
 
 season_dict = getSeasonDict()
 selected_season = st.selectbox("Select the season to analyze", list(season_dict.keys()))
+selected_season_match_data = getSelectedSeasonMatchData(selected_season)
 
 
 if selected_season:
     st.write(f"Analyzing season: {selected_season}")
 
-    # Add your analysis code here based on the selected season
-    # You can fetch and process the data for the selected season
-
-    # Example: Display a table with the data
-    data = {
-        "Date": ["2021-01-01", "2021-01-05", "2021-01-10"],
-        "Opponent": ["Real Madrid", "Atletico Madrid", "Sevilla"],
-        "Result": ["W", "L", "D"],
-    }
-
     st.write("Match Data:")
-    st.table(data)
+    st.table(selected_season_match_data)
