c4dcf31de8999b02a31d2c50695e5fa9020baaa1
PratikHotchandani <pratikhot123@live.com>
2023-07-04 15:38:31 -0400

    Added code for passing networks

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
-from mplsoccer import Sbopen
+from mplsoccer import Sbopen,Pitch
 import matplotlib.pyplot as plt
+import numpy as np
 
 parser = Sbopen()
 
@@ -105,10 +106,75 @@ def getPassesPerPlayerCount(df):
     ax.set_ylabel('Number of Passes')
 
     # Rotate the x-axis labels for better readability
-    plt.xticks(rotation=90)
-
+    plt.xticks(rotation=45)
     # Show the plot in Streamlit app
     st.pyplot(fig)
+
+def getTeamPassingNetwork(df):
+    df_pass = df[['x', 'y', 'end_x', 'end_y', "player_name", "pass_recipient_name"]]
+    #adjusting that only the surname of a player is presented.
+    df_pass["player_name"] = df_pass["player_name"].apply(lambda x: str(x).split()[-1])
+    df_pass["pass_recipient_name"] = df_pass["pass_recipient_name"].apply(lambda x: str(x).split()[-1])
+    scatter_df = pd.DataFrame()
+    for i, name in enumerate(df_pass["player_name"].unique()):
+        passx = df_pass.loc[df_pass["player_name"] == name]["x"].to_numpy()
+        recx = df_pass.loc[df_pass["pass_recipient_name"] == name]["end_x"].to_numpy()
+        passy = df_pass.loc[df_pass["player_name"] == name]["y"].to_numpy()
+        recy = df_pass.loc[df_pass["pass_recipient_name"] == name]["end_y"].to_numpy()
+        scatter_df.at[i, "player_name"] = name
+        #make sure that x and y location for each circle representing the player is the average of passes and receptions
+        scatter_df.at[i, "x"] = np.mean(np.concatenate([passx, recx]))
+        scatter_df.at[i, "y"] = np.mean(np.concatenate([passy, recy]))
+        #calculate number of passes
+        scatter_df.at[i, "no"] = df_pass.loc[df_pass["player_name"] == name].count().iloc[0]
+
+    #adjust the size of a circle so that the player who made more passes
+    scatter_df['marker_size'] = (scatter_df['no'] / scatter_df['no'].max() * 1500)
+    #counting passes between players
+    df_pass["pair_key"] = df_pass.apply(lambda x: "_".join(sorted([x["player_name"], x["pass_recipient_name"]])), axis=1)
+    lines_df = df_pass.groupby(["pair_key"]).x.count().reset_index()
+    lines_df.rename({'x':'pass_count'}, axis='columns', inplace=True)
+    #setting a treshold. You can try to investigate how it changes when you change it.
+    lines_df = lines_df[lines_df['pass_count']>2]
+
+    #plot once again pitch and vertices
+    pitch = Pitch(line_color='grey')
+    fig, ax = pitch.grid(grid_height=0.9, title_height=0.06, axis=False,
+                        endnote_height=0.04, title_space=0, endnote_space=0)
+    pitch.scatter(scatter_df.x, scatter_df.y, s=scatter_df.marker_size, color='red', edgecolors='grey', linewidth=1, alpha=1, ax=ax["pitch"], zorder = 3)
+    for i, row in scatter_df.iterrows():
+        pitch.annotate(row.player_name, xy=(row.x, row.y), c='black', va='center', ha='center', weight = "bold", size=16, ax=ax["pitch"], zorder = 4)
+
+    for i, row in lines_df.iterrows():
+            player1 = row["pair_key"].split("_")[0]
+            player2 = row['pair_key'].split("_")[1]
+            #take the average location of players to plot a line between them
+            player1_x = scatter_df.loc[scatter_df["player_name"] == player1]['x'].iloc[0]
+            player1_y = scatter_df.loc[scatter_df["player_name"] == player1]['y'].iloc[0]
+            player2_x = scatter_df.loc[scatter_df["player_name"] == player2]['x'].iloc[0]
+            player2_y = scatter_df.loc[scatter_df["player_name"] == player2]['y'].iloc[0]
+            num_passes = row["pass_count"]
+            #adjust the line width so that the more passes, the wider the line
+            line_width = (num_passes / lines_df['pass_count'].max() * 10)
+            #plot lines on the pitch
+            pitch.lines(player1_x, player1_y, player2_x, player2_y,
+                            alpha=1, lw=line_width, zorder=2, color="red", ax = ax["pitch"])
+
+    fig.suptitle("Baracelona Passing Network against" + selected_opponent, fontsize = 30)
+    st.pyplot(fig)
+
+
+
+
+def getPassingNetwork(df):
+    ## for Barcelona
+    barca_passes = df[df['team_name'] == 'Barcelona']
+    getTeamPassingNetwork(barca_passes)
+
+    ## for selected opponent
+    #opponent_passes = df[df['team_name'] == selected_opponent]
+    #getTeamPassingNetwork(opponent_passes)
+
 
 
 
@@ -122,11 +188,10 @@ if selected_season:
 
 if selected_opponent:
     st.write(f"Analyzing opponent: {selected_opponent}")
-    st.write("Match Data:")
-    st.table(selected_opponent_matches)
 
 if selected_analysis:
     st.write(f"Analyzing : {selected_analysis}")
     if selected_analysis == "Passes":
         getPassesPerPlayerCount(completed_normal_passes)
+        getPassingNetwork(completed_normal_passes)
 
