e85bd9be08467de38635b9626fe9c68d8ad30614
PratikHotchandani <pratikhot123@live.com>
2023-07-06 00:51:18 -0400

    passing heatmap code

--- main.py
+++ main.py
@@ -230,6 +230,53 @@ def getPlayersPassesPlot(df):
     else:
         st.write("Player had zero passes throughout the game, this could also mean that player was not substituted in the match or was not in the starting eleven")
 
+def getPassingHeatMap(df):
+    #declare an empty dataframe
+    danger_passes = pd.DataFrame()
+    #open the event data from this game
+    df = parser.event(idx)[0]
+    for period in [1, 2]:
+        #keep only accurate passes by England that were not set pieces in this period
+        mask_pass = (df.team_name == team) & (df.type_name == "Pass") & (df.outcome_name.isnull()) & (df.period == period) & (df.sub_type_name.isnull())
+        #keep only necessary columns
+        passes = df.loc[mask_pass, ["x", "y", "end_x", "end_y", "minute", "second", "player_name"]]
+        #keep only Shots by England in this period
+        mask_shot = (df.team_name == team) & (df.type_name == "Shot") & (df.period == period)
+        #keep only necessary columns
+        shots = df.loc[mask_shot, ["minute", "second"]]
+        #convert time to seconds
+        shot_times = shots['minute']*60+shots['second']
+        shot_window = 15
+        #find starts of the window
+        shot_start = shot_times - shot_window
+        #condition to avoid negative shot starts
+        shot_start = shot_start.apply(lambda i: i if i>0 else (period-1)*45)
+        #convert to seconds
+        pass_times = passes['minute']*60+passes['second']
+        #check if pass is in any of the windows for this half
+        pass_to_shot = pass_times.apply(lambda x: True in ((shot_start < x) & (x < shot_times)).unique())
+
+        #keep only danger passes
+        danger_passes_period = passes.loc[pass_to_shot]
+        #concatenate dataframe with a previous one to keep danger passes from the whole tournament
+        danger_passes = pd.concat([danger_passes, danger_passes_period], ignore_index = True)
+
+
+        #plot vertical pitch
+    pitch = Pitch(line_zorder=2, line_color='black')
+    fig, ax = pitch.grid(grid_height=0.9, title_height=0.06, axis=False,
+                        endnote_height=0.04, title_space=0, endnote_space=0)
+    #get the 2D histogram
+    bin_statistic = pitch.bin_statistic(danger_passes.x, danger_passes.y, statistic='count', bins=(6, 5), normalize=False)
+    #normalize by number of games
+    bin_statistic["statistic"] = bin_statistic["statistic"]
+    #make a heatmap
+    pcm  = pitch.heatmap(bin_statistic, cmap='Reds', edgecolor='grey', ax=ax['pitch'])
+    #legend to our plot
+    ax_cbar = fig.add_axes((1, 0.093, 0.03, 0.786))
+    cbar = plt.colorbar(pcm, cax=ax_cbar)
+    fig.suptitle('Danger passes by ' + team + " per game", fontsize = 30)
+    st.pyplot(fig)
 
 
 
@@ -247,6 +294,14 @@ if selected_opponent:
 if selected_analysis:
     st.write(f"Analyzing : {selected_analysis}")
     if selected_analysis == "Passes":
+
+        if selected_player:
+            st.write(f"Analyzing passes for {selected_player} against {selected_opponent}")
+            st.write("1. Home Game")
+            getPlayersPassesPlot(home_events)
+            st.write("2. Away Game")
+            getPlayersPassesPlot(away_events)
+
         st.write(f"Analyzing total passes for both home and away games:")
         getPassesPerPlayerCount(completed_normal_passes)
 
@@ -259,14 +314,10 @@ if selected_analysis:
         st.write(f"Passing network of Barcelona against: {selected_opponent}")
         getPassingNetwork(selected_opponent_match_events)
 
-        st.write("Passing network of Barcelona for away game")
-        #getPassingNetwork(away_events)
+        st.write("Passing heatmap - Most dangerous passes heatmap at home")
+        getPassingHeatMap(home_events)
 
-        if selected_player:
-            st.write(f"Analyzing passes for {selected_player} against {selected_opponent}")
-            st.write("1. Home Game")
-            getPlayersPassesPlot(home_events)
-            st.write("2. Away Game")
-            getPlayersPassesPlot(away_events)
+        st.write("Passing heatmap - Most dangerous passes heatmap at away")
+        getPassingHeatMap(away_events)
 
 
